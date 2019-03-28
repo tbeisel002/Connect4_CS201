e17ddd9ddc78cc29af7f0aad99cf70e91905727d
Tyler Beisel <tbeisel002@gmail.com>
2019-03-27 23:47:34 -0500

    Final comments for bot.c

--- bot.c
+++ bot.c
@@ -3,7 +3,7 @@
 * This file contains the 2 recursive min and max depth first seach algorithms
 * that are used to determine the next best move for the ai. The main method in
 * main.c calls minAlg, and then minAlg and maxAlg call each other recursively until
-* the best move is found up to a depth of 6. Once a depth of 6 is reached, the
+* the best move is found, or up to a depth of 6. Once a depth of 6 is reached, the
 * the lower bound for possible scores if the search down that path were to continue
 * is returned. The algorithm uses an alpha beta variation of minimax (which means
 * that if a searches upper bound is less than a move's score thats already been found,
@@ -25,7 +25,31 @@
 static int * maxAlg(int *board, int size, int depth, int moveCount, int col, int height,
   int alpha, int beta, int colOrder[]);
 
-
+/*
+* minAlg Function
+* *board - the 2d array that contains the current connect 4 board
+* size - the size of the board. Eg 10 for a 10x10 board
+* depth - the depth of the recursive call (first call is 0)
+* moveCount - how many moves have been played. Keeping this as a parameter instead
+              of computing it each time when evaluating end game state to determine
+              when a tie is reached speeds up run time.
+* col - the column that has just been played
+* height - the row that most recent token was played in (same as moveCount)
+* alpha - the lower bound for score that were searching for
+* beta - the upper bound for score that were searching for
+* colOrder[] - the altered column order to search (middle out). See top of file
+               description for why this is so beneficial.
+* return - an array in the form of [columnOfBestScore, bestScore]
+****
+****
+* minAlg simulates the cpu playing in each column and recursively calls
+* maxAlg to simulate the users play. It always picks the lowest
+* possible score where the user winning = (size * size) - depth and the cpu
+* winning = depth - (size * size). Once a depth of 6 is reached, the upper bound
+* for the score down that path is returned. If a paths upper bound is lower than
+* the lowest score found yet. That upper bound is returned and the rest of the
+* search is pruned off.
+*/
 int * minAlg(int *board, int size, int depth, int moveCount, int col, int height,
   int alpha, int beta, int colOrder[]) {
   static int baseMinArr[2]; //static array to be retuned if base case met
@@ -34,19 +58,19 @@ int * minAlg(int *board, int size, int depth, int moveCount, int col, int height
   int baseMinArrInternal[2]; //non-static array for base case operations within function
   int gameOver = checkGameState((int *)board, size, col, height);
   *baseMinArrInternal = col;
-  if(gameOver == 1) {
+  if(gameOver == 1) { // if the user has won (base case)
     *(baseMinArrInternal+1) = (size * size) - depth;
     baseMinArr[0] = *baseMinArrInternal;
     baseMinArr[1] = *(baseMinArrInternal+1);
     return baseMinArr;
   }
-  else if(gameOver == 2) {
+  else if(gameOver == 2) { // if the cpu has won (base case)
     *(baseMinArrInternal+1)  = depth - (size * size);
     baseMinArr[0] = *baseMinArrInternal;
     baseMinArr[1] = *(baseMinArrInternal+1);
     return baseMinArr;
   }
-  else if(moveCount >= size * size) {
+  else if(moveCount >= size * size) { // if its a tie (base case)
     *(baseMinArrInternal+1)  = 0;
     baseMinArr[0] = *baseMinArrInternal;
     baseMinArr[1] = *(baseMinArrInternal+1);
@@ -55,14 +79,14 @@ int * minAlg(int *board, int size, int depth, int moveCount, int col, int height
   *min = -2;
   *(min+1) = (size * size) - (depth+1); //upper bound for score
   if(beta > *(min+1)) {
-    beta = *(min+1);
-    if(beta <= alpha) {
+    beta = *(min+1); // if beta > upper bound, beta = upper bound
+    if(beta <= alpha) { // if the upper bound <= previousky found lower bound, return (prune)
       baseMinArr[0] = *min;
       baseMinArr[1] = beta;
       return baseMinArr;
     }
   }
-  if(depth > 6) {
+  if(depth > 6) { // if depth of 6 reached, return upper bound (base case)
     baseMinArr[0] = *baseMinArrInternal;
     baseMinArr[1] = (size * size) - (depth+1);
     return baseMinArr;
@@ -71,22 +95,21 @@ int * minAlg(int *board, int size, int depth, int moveCount, int col, int height
   int i, colHeight;
   for(i = 0; i < size; i++) {
     colHeight = colFull((int *)board, colOrder[i], size);
-      if(colHeight != -1) {
-        //int board2 [size][size];
-        int *board2 = (int*)malloc(sizeof(int)*size*size);
-        memcpy(board2, (int *)board, sizeof (int) * size * size);
-        placeMove((int *)board2, 2, colOrder[i], size);
+      if(colHeight != -1) { // if the column is not already full
+        int *board2 = (int*)malloc(sizeof(int)*size*size); // make a copy of the board and
+        memcpy(board2, (int *)board, sizeof (int) * size * size); // place token in
+        placeMove((int *)board2, 2, colOrder[i], size); // centermost unsearched column
 
         int *minCurr = maxAlg((int *)board2, size, depth+1, moveCount+1, colOrder[i],
-        colHeight, alpha, beta, colOrder);
-        if(*(minCurr+1) <= alpha) {
-          returnMin[1] = *(minCurr+1);
+        colHeight, alpha, beta, colOrder); // recursive call to simulate user after cpu plays
+        if(*(minCurr+1) <= alpha) { // if returned score is lower than previous lower
+          returnMin[1] = *(minCurr+1); // lower bound. Return returned score (prune)
           returnMin[0] = colOrder[i];
           free(board2);
           return returnMin;
         }
-        else if(*(minCurr+1) < beta) {
-          beta = *(minCurr+1);
+        else if(*(minCurr+1) < beta) { // if returned score is less than previous
+          beta = *(minCurr+1); // upper bound, update new upper bound to returned score
           *min = colOrder[i];
         }
         free(board2);
@@ -97,7 +120,31 @@ int * minAlg(int *board, int size, int depth, int moveCount, int col, int height
   return returnMin;
 }
 
-
+/*
+* maxAlg Function
+* *board - the 2d array that contains the current connect 4 board
+* size - the size of the board. Eg 10 for a 10x10 board
+* depth - the depth of the recursive call (first call is 0)\
+* moveCount - how many moves have been played. Keeping this as a parameter instead
+              of computing it each time when evaluating end game state to determine
+              when a tie is reached speeds up run time.
+* col - the column that has just been played
+* height - the row that most recent token was played in
+* alpha - the lower bound for score that were searching for
+* beta - the upper bound for score that were searching for
+* colOrder[] - the altered column order to search (middle out). See top of file
+               description for why this is so beneficial.
+* return - an array in the form of [columnOfBestScore, bestScore]
+****
+****
+* maxAlg simulates the user playing in each column and recursively calls
+* minAlg to simulate the cpu's play. It always picks the highest
+* possible score where the user winning = (size * size) - depth and the cpu
+* winning = depth - (size * size). Once a depth of 6 is reached, the lower bound
+* for the score down that path is returned. If a paths lower bound is higher than
+* the highest score found yet. That upper bound is returned and the rest of the
+* search is pruned off.
+*/
 static int * maxAlg(int *board, int size, int depth, int moveCount, int col, int height,
   int alpha, int beta, int colOrder[]) {
   static int baseMaxArr[2]; //static array to be retuned if base case met
@@ -106,35 +153,35 @@ static int * maxAlg(int *board, int size, int depth, int moveCount, int col, int
   int baseMaxArrInternal[2]; // non-static array for base case operations within function
   int gameOver = checkGameState((int *)board, size, col, height);
   *baseMaxArrInternal = col;
-  if(gameOver == 1) {
+  if(gameOver == 1) { // if the user has won (base case)
     *(baseMaxArrInternal+1) = (size * size) - depth;
     baseMaxArr[0] = *baseMaxArrInternal;
     baseMaxArr[1] = *(baseMaxArrInternal+1);
     return baseMaxArr;
   }
-  else if(gameOver == 2) {
+  else if(gameOver == 2) { // if the cpu has won (base case)
     *(baseMaxArrInternal+1) = depth - (size * size);
     baseMaxArr[0] = *baseMaxArrInternal;
     baseMaxArr[1] = *(baseMaxArrInternal+1);
     return baseMaxArr;
   }
-  else if(moveCount >= size * size) {
+  else if(moveCount >= size * size) { // if its a tie (base case)
     *(baseMaxArrInternal+1) = 0;
     baseMaxArr[0] = *baseMaxArrInternal;
     baseMaxArr[1] = *(baseMaxArrInternal+1);
     return baseMaxArr;
   }
   *(max) = -1;
-  *(max+1) = (depth+1) - (size * size);
+  *(max+1) = (depth+1) - (size * size); // lower bound for score
   if(alpha > *(max+1)) {
-    alpha = *(max+1);
-    if(alpha >= beta) { // prune this exploration
+    alpha = *(max+1); // if alpha > lower bound, alpha = lower bound
+    if(alpha >= beta) { // if the lower bound >= previously found upper bound,
       baseMaxArr[0] = *max;
       baseMaxArr[1] = beta;
-      return baseMaxArr;
+      return baseMaxArr; // return lower bound (prune)
     }
   }
-  if(depth > 6) { // Only go up to depth of 5 (0-4)
+  if(depth > 6) { // if depth of 6 reached, return lower bound (base case)
     baseMaxArr[0] = *baseMaxArrInternal;
     baseMaxArr[1] = (depth+1) - (size * size);
     return baseMaxArr;
@@ -143,23 +190,22 @@ static int * maxAlg(int *board, int size, int depth, int moveCount, int col, int
   int i, colHeight;
   for(i = 0; i < size; i++) {
     colHeight = colFull((int *)board, colOrder[i], size);
-    if(colHeight != -1) {
-      //int board2 [size][size];
+    if(colHeight != -1) { // if the column is not already full
       int *board2 = (int*)malloc(sizeof(int)*size*size);
       memcpy(board2, (int *)board, sizeof (int) * size * size);
       placeMove((int *)board2, 1, colOrder[i], size);
 
       int *maxCurr = minAlg((int *)board2, size, depth+1, moveCount+1, colOrder[i],
-      colHeight, alpha, beta, colOrder);
+      colHeight, alpha, beta, colOrder); // recursive call to simulate cpu after simulated user play
 
-      if(*(maxCurr+1) >= beta) { //prune if better than what were looking for
+      if(*(maxCurr+1) >= beta) { //prune if returned score better than what were looking for
         returnMax[1] = *(maxCurr+1);
         returnMax[0] = colOrder[i];
         free(board2);
         return returnMax;
       }
-      else if(*(maxCurr+1) > alpha) {
-        alpha = *(maxCurr+1);
+      else if(*(maxCurr+1) > alpha) { // if returned score is greater than previous
+        alpha = *(maxCurr+1); // upper bound, Update upper bound
         *max = colOrder[i];
       }
       free(board2);
